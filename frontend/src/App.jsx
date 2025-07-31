c3be3b60a59e5c3bbf300ae7cef7ef5ebdfc9653
Andy-K-Taua <>
2025-07-31 19:49:53 +1000

    updated pathway

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -2,7 +2,7 @@
 
 import { BrowserRouter, Routes, Route } from 'react-router-dom';
 import Login from './pages/Login';
-import Signup from './pages/Signup';
+// import Signup from './pages/Signup';
 import OriginDash from './pages/OriginDash'; 
 import RecipientDash from './pages/RecipientDash';
 
@@ -10,7 +10,8 @@ function App() {
   return (
     <BrowserRouter>
       <Routes>
-        <Route path="/" element={<Signup />} />
+        <Route path="/" element={<OriginDash />} />
+   {/* <Route path="/" element={<Signup />} /> */}
         <Route path="/login" element={<Login />} />
         <Route path="/dash/origin" element={<OriginDash />} />
         <Route path="/dash/recipient" element={<RecipientDash />} />
